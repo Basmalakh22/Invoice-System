98eb71ed0e0865e816c50ab42a347560136a286e
Basmala Khaled <basmalakha772@gmail.com>
2024-10-28 17:54:05 +0300

    Create_invoices_Table & controller & Model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -34,6 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('/invoices', InvoicesController::class);
 
 
 require __DIR__.'/auth.php';
