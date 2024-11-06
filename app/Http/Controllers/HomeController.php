29dc48a67c5191422eaf38bd14d81fab2599a632
Basmala Khaled <basmalakha772@gmail.com>
2024-11-06 22:24:25 +0200

    Statistical Home Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Invoices;
 use Illuminate\Http\Request;
+use App\Models\Invoices;
 
 class HomeController extends Controller
 {
@@ -24,6 +24,58 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        // Helper function to calculate percentage
+        $calculatePercentage = function ($count, $total) {
+            return $total == 0 ? 0 : ($count / $total) * 100;
+        };
+
+        $count_all = Invoices::count();
+        $count_invoices1 = Invoices::where('Value_Status', 1)->count();
+        $count_invoices2 = Invoices::where('Value_Status', 2)->count();
+        $count_invoices3 = Invoices::where('Value_Status', 3)->count();
+
+        $nspainvoices1 = $calculatePercentage($count_invoices1, $count_all);
+        $nspainvoices2 = $calculatePercentage($count_invoices2, $count_all);
+        $nspainvoices3 = $calculatePercentage($count_invoices3, $count_all);
+
+
+        $chartjs = app()->chartjs
+            ->name('barChartTest')
+            ->type('bar')
+            ->size(['width' => 350, 'height' => 200])
+            ->labels(['الفواتير الغير المدفوعة', 'الفواتير المدفوعة', 'الفواتير المدفوعة جزئيا'])
+            ->datasets([
+                [
+                    "label" => "الفواتير الغير المدفوعة",
+                    'backgroundColor' => ['#ec5858'],
+                    'data' => [$nspainvoices2]
+                ],
+                [
+                    "label" => "الفواتير المدفوعة",
+                    'backgroundColor' => ['#81b214'],
+                    'data' => [$nspainvoices1]
+                ],
+                [
+                    "label" => "الفواتير المدفوعة جزئيا",
+                    'backgroundColor' => ['#ff9642'],
+                    'data' => [$nspainvoices3]
+                ],
+            ])
+            ->options([]);
+
+        $chartjs_2 = app()->chartjs
+            ->name('pieChartTest')
+            ->type('pie')
+            ->size(['width' => 340, 'height' => 200])
+            ->labels(['الفواتير الغير المدفوعة', 'الفواتير المدفوعة', 'الفواتير المدفوعة جزئيا'])
+            ->datasets([
+                [
+                    'backgroundColor' => ['#ec5858', '#81b214', '#ff9642'],
+                    'data' => [$nspainvoices2, $nspainvoices1, $nspainvoices3]
+                ]
+            ])
+            ->options([]);
+
+        return view('home', compact('chartjs', 'chartjs_2'));
     }
 }
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,62 +1,87 @@
 @extends('layouts.master2')
+
+@section('title')
+تسجيل حساب جديد - برنامج الفواتير
+@endsection
+
 @section('css')
-<!-- Sidemenu-respoansive-tabs css -->
+<!-- Sidemenu-responsive-tabs css -->
 <link href="{{URL::asset('assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}" rel="stylesheet">
 @endsection
+
 @section('content')
-		<div class="container-fluid">
-			<div class="row no-gutter">
-				<!-- The image half -->
-				<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
-					<div class="row wd-100p mx-auto text-center">
-						<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
-							<img src="{{URL::asset('assets/img/media/login.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
-						</div>
-					</div>
-				</div>
-				<!-- The content half -->
-				<div class="col-md-6 col-lg-6 col-xl-5 bg-white">
-					<div class="login d-flex align-items-center py-2">
-						<!-- Demo content-->
-						<div class="container p-0">
-							<div class="row">
-								<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
+	<div class="container-fluid">
+		<div class="row no-gutter">
+			<!-- The content half -->
+			<div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+				<div class="login d-flex align-items-center py-2">
+					<div class="container p-0">
+						<div class="row">
+							<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
+								<div class="card-sigin">
+									<div class="mb-5 d-flex">
+										<a href="{{ route('dashboard') }}">
+											<img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo">
+										</a>
+										<h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Invoice System</h1>
+									</div>
 									<div class="card-sigin">
-										<div class="mb-5 d-flex"> <a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Va<span>le</span>x</h1></div>
 										<div class="main-signup-header">
-											<h2 class="text-primary">Get Started</h2>
-											<h5 class="font-weight-normal mb-4">It's free to signup and only takes a minute.</h5>
-											<form action="#">
+											<h2>مرحبا بك</h2>
+											<h5 class="font-weight-semibold mb-4">حساب جديد</h5>
+											<form method="POST" action="{{ route('register') }}">
+												@csrf
 												<div class="form-group">
-													<label>Firstname &amp; Lastname</label> <input class="form-control" placeholder="Enter your firstname and lastname" type="text">
+													<label>الاسم </label>
+													<input class="form-control @error('name') is-invalid @enderror" placeholder="أدخل اسمك " type="text" name="name" value="{{ old('name') }}" required>
+													@error('name')
+														<span class="invalid-feedback">{{ $message }}</span>
+													@enderror
+												</div>
+
+												<div class="form-group">
+													<label>البريد الإلكتروني</label>
+													<input class="form-control @error('email') is-invalid @enderror" placeholder="أدخل بريدك الإلكتروني" type="email" name="email" value="{{ old('email') }}" required>
+													@error('email')
+														<span class="invalid-feedback">{{ $message }}</span>
+													@enderror
 												</div>
 												<div class="form-group">
-													<label>Email</label> <input class="form-control" placeholder="Enter your email" type="text">
+													<label>كلمة المرور</label>
+													<input class="form-control @error('password') is-invalid @enderror" placeholder="أدخل كلمة المرور" type="password" name="password" required>
+													@error('password')
+														<span class="invalid-feedback">{{ $message }}</span>
+													@enderror
 												</div>
 												<div class="form-group">
-													<label>Password</label> <input class="form-control" placeholder="Enter your password" type="password">
-												</div><button class="btn btn-main-primary btn-block">Create Account</button>
-												<div class="row row-xs">
-													<div class="col-sm-6">
-														<button class="btn btn-block"><i class="fab fa-facebook-f"></i> Signup with Facebook</button>
-													</div>
-													<div class="col-sm-6 mg-t-10 mg-sm-t-0">
-														<button class="btn btn-info btn-block"><i class="fab fa-twitter"></i> Signup with Twitter</button>
-													</div>
+													<label>تأكيد كلمة المرور</label>
+													<input class="form-control @error('password_confirmation') is-invalid @enderror" placeholder="أعد إدخال كلمة المرور" type="password" name="password_confirmation" required>
+													@error('password_confirmation')
+														<span class="invalid-feedback">{{ $message }}</span>
+													@enderror
 												</div>
+												<button class="btn btn-main-primary btn-block">إنشاء حساب</button>
 											</form>
-											<div class="main-signup-footer mt-5">
-												<p>Already have an account? <a href="{{ url('/' . $page='signin') }}">Sign In</a></p>
-											</div>
+											
 										</div>
 									</div>
 								</div>
 							</div>
-						</div><!-- End -->
+						</div>
+					</div><!-- End -->
+				</div>
+			</div><!-- End -->
+
+			<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+				<div class="row wd-100p mx-auto text-center">
+					<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+						<img src="{{URL::asset('assets/img/media/login.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
 					</div>
-				</div><!-- End -->
+				</div>
 			</div>
 		</div>
+	</div>
 @endsection
+
 @section('js')
 @endsection
